43d04050cc9227fbdac6eab8be122e821c6a0fa4
Wetia <jojo2schmitz@web.de>
2019-08-06 00:23:03 +0200

    Removed temp import files Add temp import files to ignore list
    Adapt map, map_node and static_game_object -class

--- scenen/map/map.gd
+++ scenen/map/map.gd
@@ -1,21 +1,98 @@
-extends "res://scenen/StaticGameObject/StaticGameObject.gd"
+extends Spatial
+class_name class_map, "res://scenen/map/map.gd"
 
-var map = []
+var map_nodes = []
+var map_fog_of_war_nodes = []
 
+var map_size : Vector3
 
-func _ready():    
-    pass
-                
-func ini_map(map_size : Vector3):
-    for x in range(map_size.x):
-        map[x].append([])
-        for y in range(map_size.y):
-            map[x][y].append([])
-            for z in range(map_size.z):
-                map[x][y][z].append(null)
-                
-func get_map_node(position : Vector3) -> class_StaticGameObject:
-    return map[position.x][position.y][position.z]
+func _ready():
+    pass                       
+                                
+func get_map_node(Position : Vector3) -> class_map_node:
+    var map_size : Vector3 = get_map_size()
+    if Position.x < 0 or Position.x >= map_size.x or \
+       Position.y < 0 or Position.y >= map_size.y or \
+       Position.z < 0 or Position.z >= map_size.z:
+        return null        
+    return map_nodes[Position.x][Position.y][Position.z]
     
-func set_map(var Map):
-    map = Map
+func get_map_fog_of_war_node(Position : Vector3) -> class_map_node:
+    var map_size : Vector3 = get_map_size()
+    if Position.x < 0 or Position.x >= map_size.x or \
+       Position.y < 0 or Position.y >= map_size.y or \
+       Position.z < 0 or Position.z >= map_size.z:
+        return null        
+    return map_fog_of_war_nodes[Position.x][Position.y][Position.z]
+   
+func get_map_size() -> Vector3:
+    return Vector3(map_nodes.size(),map_nodes[0].size(),map_nodes[0][0].size())
+
+func get_active_nodes() -> Array:
+    var active_nodes = self.get_node("active_nodes").get_children()    
+    return active_nodes 
+
+func activate_node(Pos : Vector3):
+    var node : class_map_node = get_map_node(Pos)
+    if node.active == true:
+        return
+    node.active = true
+    self.get_node("active_nodes").add_child(node)
+    self.get_node("fog_of_war_nodes").remove_child(get_map_fog_of_war_node(Pos))
+     
+func deactivate_node(Pos : Vector3):
+    var node : class_map_node = get_map_node(Pos)
+    node.active = false
+    self.get_node("active_nodes").remove_child(node)
+    self.get_node("fog_of_war_nodes").add_child(get_map_fog_of_war_node(Pos))
+    
+func set_map_nodes(var Map_nodes):
+    map_nodes = Map_nodes
+    
+func get_node_neighbours(Node_pos : Vector3) -> Array:
+    var neighbours = []
+    if get_map_node(Node_pos + Vector3(-1,0,0)):
+        neighbours.append(get_map_node(Node_pos + Vector3(-1,0,0)))
+    if get_map_node(Node_pos + Vector3(0,-1,0)):
+        neighbours.append(get_map_node(Node_pos + Vector3(0,-1,0)))
+    if get_map_node(Node_pos + Vector3(0,0,-1)):
+        neighbours.append(get_map_node(Node_pos + Vector3(0,0,-1)))
+    if get_map_node(Node_pos + Vector3(1,0,0)):
+        neighbours.append(get_map_node(Node_pos + Vector3(1,0,0)))
+    if get_map_node(Node_pos + Vector3(0,1,0)):
+        neighbours.append(get_map_node(Node_pos + Vector3(0,1,0)))
+    if get_map_node(Node_pos + Vector3(0,0,1)):
+        neighbours.append(get_map_node(Node_pos + Vector3(0,0,1)))    
+    return neighbours
+    
+func get_active_node_neighbours(Node_pos : Vector3) -> Array:
+    var active_neighbours = []
+    var neighbours = get_node_neighbours(Node_pos)
+    for node in neighbours:
+        if node.active:
+            active_neighbours.append(node)        
+    return active_neighbours
+  
+func get_not_active_nodes_neighbour(Node_pos : Vector3) -> Array:
+    var not_active_neighbours = []
+    var neighbours = get_node_neighbours(Node_pos)
+    for node in neighbours:
+        if not node.active:
+            not_active_neighbours.append(node)   
+    return not_active_neighbours
+     
+func init_map_fog_of_war_nodes(map_size : Vector3):
+    var class_map_node = load("res://scenen/static_game_object/static_game_object_unknown.tscn")
+    var map_node = class_map_node.instance()
+    map_fog_of_war_nodes.resize(map_size.x)
+    for x in map_size.x:
+        map_fog_of_war_nodes[x] = []
+        map_fog_of_war_nodes[x].resize(map_size.y)    # Y-dimension
+        for y in map_size.y:
+            map_fog_of_war_nodes[x][y] = []
+            map_fog_of_war_nodes[x][y].resize(map_size.z)    # Z-dimension
+            for z in map_size.z:
+                var temp_node = map_node.duplicate()
+                map_fog_of_war_nodes[x][y][z] = temp_node
+                temp_node.translation = Vector3(x,y *-1,z)
+                self.get_node("fog_of_war_nodes").add_child(temp_node)
---
+++ scenen/static_game_object/static_game_object.gd
@@ -0,0 +1,42 @@
+class_name class_StaticGameObject extends Spatial
+
+export (bool) var is_selectable_flag = true
+export (int) var life_max  = 42    
+export (Vector2) var size = Vector2(1,1)
+
+export (bool) var transparent = false
+
+
+var life_actual  = 42
+var selected = false
+
+func _ready():
+    pass # Replace with function body.
+
+func is_selectable() -> bool:
+    return is_selectable_flag
+    
+func select(flag):
+    pass
+
+func get_life_actual() -> int:
+    return life_actual
+    
+func get_life_max()  -> int:
+    return life_max
+    
+func apply_damage(damage : int) -> int:
+    life_actual -= damage
+    return life_actual
+    
+func apply_heal(heal : int) -> int:
+    life_actual += heal
+    if life_actual > life_max:
+        life_actual = life_max
+    return life_actual
+    
+func get_position() -> Vector3:
+    return self.translation
+    
+func set_position(position : Vector3):
+    self.translation = position     
