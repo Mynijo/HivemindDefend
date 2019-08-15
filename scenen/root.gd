0128c1babe1999a2d8f8d4586d6fbd7a221c2e4e
Varicus <florianlotter@web.de>
2019-08-15 08:24:24 +0200

    Made save and load happen after a few seconds

--- scenen/root.gd
+++ scenen/root.gd
@@ -2,18 +2,26 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    var strat_time = OS.get_ticks_msec()
+    var start_time = OS.get_ticks_msec()
 
     $Map.gen_map_with_file()
-
-    var map_gen_time = OS.get_ticks_msec() -strat_time
+    var map_gen_time = OS.get_ticks_msec() -start_time
     print("Gen Map:       ",map_gen_time / 1000.0, "Sec")
 
-    save_load_manager.save_game()
-    var map_save_time = OS.get_ticks_msec() -map_gen_time
-    print("save_game:     ",map_save_time / 1000.0, "Sec")
 
+func _on_SaveTimer_timeout():
+    print("SaveTimer finished, saving game...")
+    var start_time = OS.get_ticks_msec()
+    save_load_manager.save_game()
+    var map_save_time = OS.get_ticks_msec() - start_time
+    print("save_game:     ",map_save_time / 1000.0, "Sec")
+    $LoadTimer.start()
+
+
+func _on_LoadTimer_timeout():
+    print("LoadTimer finished, loading game...")
+    var start_time = OS.get_ticks_msec()
     save_load_manager.load_game()
-    var map_load_time = OS.get_ticks_msec() -map_save_time
+    var map_load_time = OS.get_ticks_msec() - start_time
     print("load_game:    ",map_load_time / 1000.0, "Sec")
-    #get_tree().get_root().print_stray_nodes()
+    get_tree().get_root().print_stray_nodes()
