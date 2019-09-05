528969311eb59b293546245152e7ba6fe29aea32
Varicus <florianlotter@web.de>
2019-09-06 01:26:32 +0200

    Block properties on mouse over, path selection by clicking

--- scenen/DebugInfo.gd
+++ scenen/DebugInfo.gd
@@ -4,9 +4,12 @@ extends Control
 # var a = 2
 # var b = "text"
 
+var path_from : Vector3 = Vector3(7,2,2)
+var path_to : Vector3 = Vector3(2,3,2)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+    $Labels/PathNodes.text = "PathNodes: " + str(path_from) + " -> " + str(path_to)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -14,9 +17,14 @@ func _ready():
 
 
 func _on_Button_pressed():
-    var nodes : PoolVector3Array = pathfinding_manager.get_node_path(Vector3(7,2,2),Vector3(2,3,2))
+    var nodes : PoolVector3Array = pathfinding_manager.get_node_path(self.path_from, self.path_to)
     var label : Label = get_tree().get_root().get_node("/root/root/DebugInfo/Labels/PathNodesValue")
     var pathAsString = ""
+    var geom = get_tree().get_root().get_node("/root/root/PathLine")
+    geom.clear()
+    geom.begin(2)
     for node in nodes:
         pathAsString = pathAsString + String(node) + "\n"
+        geom.add_vertex(node * Vector3(1, -1, 1))
+    geom.end()
     label.set_text(pathAsString)
--- scenen/root.gd
+++ scenen/root.gd
@@ -1,5 +1,8 @@
 extends Node
 
+const RAY_LENGTH = 1000
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 #    var trans
@@ -50,3 +53,29 @@ func _on_HideTimer_timeout():
         #print("Showing floor ", current_floor)
         $Map.show_floor(current_floor)
     current_floor += floor_mod
+
+var button_toggle = false
+func _input(event):
+    if event is InputEventMouse:
+        var mouse_position = event.position
+        var from_pos = $CamBase/Camera.project_ray_origin(mouse_position)
+        var to_pos = from_pos + $CamBase/Camera.project_ray_normal(mouse_position) * RAY_LENGTH
+        var hit = $Map.get_world().get_direct_space_state().intersect_ray(from_pos, to_pos)
+        var block_position
+        var nearest_air
+        if hit:
+            if hit.get("collider") == $Map/GridMap:
+                #text = hit.collider.world_to_map(hit.position) * Vector3(1, -1, 1)
+                block_position = (hit.position - 0.1 * hit.normal).round() * Vector3(1, -1, 1)
+                nearest_air = (hit.position + 0.5 * hit.normal).round() * Vector3(1, -1, 1)
+        $DebugInfo/Labels/CoordinateValue.text = str(mouse_position) + ' -> ' + str(hit.get("position")) + " [" + str(block_position) + "]"
+        $DebugInfo/Labels/BlockTypeValue.text = str($Map.map_nodes.get(block_position))
+        #$DebugInfo/Labels/BlockTypeValue.text = str(hit)
+        if event is InputEventMouseButton and event.is_pressed() and nearest_air:
+            if $DebugInfo.path_to != Vector3(-1, -1, -1):
+                $DebugInfo.path_to = Vector3(-1, -1, -1)
+                $DebugInfo.path_from = nearest_air
+                $DebugInfo/Labels/PathNodes.text = "PathNodes: " + str($DebugInfo.path_from) + " -> NULL"
+            else:
+                $DebugInfo.path_to = nearest_air
+                $DebugInfo/Labels/PathNodes.text = "PathNodes: " + str($DebugInfo.path_from) + " -> " + str($DebugInfo.path_to)
