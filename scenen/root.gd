4b194a3584d7a8738ead75a1f937201290dece6a
Varicus <florianlotter@web.de>
2019-09-05 11:43:51 +0200

    Changed debug grid and removed some debug print output

--- scenen/root.gd
+++ scenen/root.gd
@@ -2,12 +2,20 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+#    var trans
+#    var basis
+#    var orientation_id
+#    var rotation = Transform().rotated(Vector3(0, 1, 0), 2 * PI / 2).orthonormalized()
+#    for current_orientation_id in range(24):
+#        basis = $Map/MapGenerator.ORIENTATION_ARRAY[current_orientation_id % 24]
+#        trans = Transform(basis)
+#        orientation_id = (rotation * trans).basis.get_orthogonal_index()
+#        print(current_orientation_id, " -> ", orientation_id)
     var start_time = OS.get_ticks_msec()
-
+    #$Map.gen_map_with_file("res://scenen/map/test_map_generation_config.json")
     $Map.gen_map_with_file()
     var map_gen_time = OS.get_ticks_msec() -start_time
     print("Gen Map:       ",map_gen_time / 1000.0, "Sec")
-
 
 func _on_SaveTimer_timeout():
     print("SaveTimer finished, saving game...")
